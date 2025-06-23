63f6eca57a999a96961aefb1da3cc1a6907e5867
Heiko-Lemmert <heiko.lemmert91@gmail.com>
2025-06-23 21:04:31 +0200

    feat: update reviews section with dynamic content and animations, enhance styling for better user interaction

--- src/app/home/reviews/reviews.component.ts
+++ src/app/home/reviews/reviews.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-reviews',
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, CommonModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss'
 })
@@ -12,9 +13,47 @@ export class ReviewsComponent {
   reviewsData = [
     {
       name: 'Daniel N.',
-      review: 'lorem ipsum abat is ed kolom',
-      avatar: '',
+      initial: 'DN',
+      review: 'review-1',
+      project: 'Join',
+    },
+    {
+      name: 'Lars S.',
+      initial: 'LS',
+      review: 'review-2',
+      project: 'Join',
+    },
+    {
+      name: 'Daniel L.',
+      initial: 'DL',
+      review: 'review-3',
+      project: 'DA Bubble',
+    }
+  ];
+
+  reviewName = this.reviewsData[0].name;
+  reviewInitial = this.reviewsData[0].initial;
+  reviewText = this.reviewsData[0].review;
+
+  isAnimating: boolean = false;
+  nextIndex: number | null = null;
+  isHovered: boolean = false;
+
+  setReview(index: number) {
+    if (index === this.reviewsData.findIndex(r => r.name === this.reviewName) || this.isAnimating) return;
+    this.isAnimating = true;
+    this.nextIndex = index;
+    setTimeout(() => {
+      this.reviewName = this.reviewsData[index].name;
+      this.reviewInitial = this.reviewsData[index].initial;
+      this.reviewText = this.reviewsData[index].review;
+      this.isAnimating = false;
+      this.nextIndex = null;
+    }, 600);
   }
-]
+
+  setActiv(index: number) {
+    return index === this.reviewsData.findIndex(r => r.name == this.reviewName);
+  }
 
 }
