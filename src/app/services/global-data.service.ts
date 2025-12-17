9cedaac0eb355ed614beb3298423c47e41f14f04
Heiko-Lemmert <heiko.lemmert91@gmail.com>
2025-12-17 17:07:38 +0100

    fix: Domain Name in GlobalData Service

--- src/app/services/global-data.service.ts
+++ src/app/services/global-data.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GlobalDataService {
-  readonly domain: string = 'https://lemmert.dev';
+  readonly domain: string = 'https://heiko.lemmert.dev';
   readonly mail: string = 'heiko.lemmert@outlook.com';
   readonly address: string = `Heiko Lemmert<br>Rudolf-Diesel-Str. 11 A<br>89257 Illertissen`
   private _lightModeActivated:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
